24114f71d91f26ccb55a5996e6675b79b9b19919
sabermahmud <sabermahmud.com>
2025-11-10 04:37:07 +0300

    fire base installed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
                     <ul
                         tabIndex="-1"
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
+                        <li><a></a></li>
                         <li>
                             <a>Parent</a>
                             <ul className="p-2">
@@ -22,10 +22,11 @@ const Navbar = () => {
                         <li><a>Item 3</a></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl"><img src="/src/assets/images/logo.png" className='w-25 p-2 hidden lg:flex' alt="" /></a>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+            <div className="navbar-center  lg:flex">
+                <a className="btn btn-ghost text-xl"><img src="/src/assets/images/logo.png" className='w-25 p-2 lg:hidden' alt="" /></a>
+                <ul className="menu hidden lg:flex menu-horizontal px-1">
                     <li><a>Item 1</a></li>
                     <li>
                         <details>
